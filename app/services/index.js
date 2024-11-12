e151ceb9d49152dd6075430ac1896b8ae5ae4eff
Antonio Barreiro <antonio.barreiro.dw@immune.institute>
2024-11-12 20:21:27 +0100

    update index.js

--- app/services/index.js
+++ app/services/index.js
@@ -1,14 +1,27 @@
-const base = process.env.NEXT_PUBLIC_API_URL
 import axios from 'axios';
 import Cookies from 'js-cookie'
 
 class AxiosConfig {
     constructor(path) {
-        this.axios = axios.create(
-            {
-              baseURL: `${base}/${path}`
+        
+        const baseUrl = (process.env.NEXT_PUBLIC_API_URL || '').replace(/\/+$/, '');
+        
+       
+        const cleanPath = (path || '').replace(/^\/+/, '');
+        
+        this.axios = axios.create({
+            
+            baseURL: `${baseUrl}/${cleanPath}`.replace(/\/+/g, '/')
+        });
+
+        
+        this.axios.interceptors.request.use((config) => {
+            const token = this.getToken();
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
             }
-        );
+            return config;
+        });
     }
 
     getToken() {
@@ -16,4 +29,4 @@ class AxiosConfig {
     }
 }
 
-export  default AxiosConfig;
+export default AxiosConfig;
